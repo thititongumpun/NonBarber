84a64df3e48b2c6824bd73d11b16f95b38b77602
thititongumpun <thiti180536@gmail.com>
2023-03-07 17:20:19 +0700

    add redirect url

--- src/Auth/auth0-provider.tsx
+++ src/Auth/auth0-provider.tsx
@@ -28,7 +28,7 @@ export const Auth0ProviderWithNavigate = ({ children }: Auth0ProviderProps) => {
       clientId={clientId}
       authorizationParams={{
         audience: audience,
-        redirect_uri: window.location.pathname,
+        redirect_uri: redirectUri,
       }}
       onRedirectCallback={() => onRedirectCallback}
     >
