e507a1540e84fb2d50e915911eb9bcddae747220
thititongumpun <thiti180536@gmail.com>
2023-03-22 16:26:58 +0700

    feat(web) add i18n translate

--- src/api/reserveApi.ts
+++ src/api/reserveApi.ts
@@ -2,7 +2,6 @@ import { authAPI } from "./baseApi";
 import { ReserveSchemaType } from "./schemas/reserveSchema";
 
 export const createReserve = async (accessToken: string, data: ReserveSchemaType) => {
-  console.log(accessToken)
   const response = await authAPI.post<string>('api/reserve', data, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
